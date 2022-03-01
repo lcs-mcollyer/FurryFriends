0a0970e1af9f451e23224108952d8e58b0e7953b
matt collyer <mcollyer@lcs.on.ca>
2022-03-01 09:53:17 -0500

    now using the provided JSON decoder. Begin to list stored properties and the corresponding computing properties.

--- FurryFriends/Views/ContentView.swift
+++ FurryFriends/Views/ContentView.swift
@@ -11,9 +11,11 @@ struct ContentView: View {
     
     // MARK: Stored properties
     
+    
+    @State var currentDogAddedToSaved: Bool = false
     // Address for main image
     // Starts as a transparent pixel – until an address for an animal's image is set
-    @State var currentImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
+    @State var currentDog = URL(string: "https://images.dog.ceo/breeds/labrador/lab_young.JPG")!
     
     // MARK: Computed properties
     var body: some View {
@@ -21,16 +23,14 @@ struct ContentView: View {
         VStack {
             
             // Shows the main image
-            RemoteImageView(fromURL: currentImage)
+            RemoteImageView(fromURL: currentDog)
                 .padding()
-           
             
-            Button(action: {
-                
-            },label: {
-                Text("next dog")
-                })
-            buttonStyle(.bordered)
+            
+            //            Button(action: {} ,label: {
+            //                Text("next dog")
+            //                })
+            //            buttonStyle(.bordered)
             
             
             
@@ -43,13 +43,17 @@ struct ContentView: View {
                     .font(.largeTitle)
             }
             
-        
             
+            //            .multilineTextAlignment(.leading)
             Text("Saved")
                 .font(.title)
+            
             Text("List of dogs to be filled")
                 .font(.caption)
-            
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.primary, lineWidth: 4)
+                )
             // Push main grouping
             Spacer()
         }
@@ -70,7 +74,7 @@ struct ContentView: View {
             
             // Replaces the transparent pixel image with an actual image of an animal
             // Adjust according to your preference ☺️
-            currentImage = URL(string: remoteDogImage)!
+            currentDog = URL(string: remoteDogImage)!
             
             
             
@@ -79,7 +83,39 @@ struct ContentView: View {
     }
     
     // MARK: Functions
-    
+    func loadNewDog() async {
+        let url = URL(string: "")!
+        
+        var request = URLRequest(url: url)
+        // Request the JSON data
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Accept")
+        // Start a session to interact (talk with) the endpoint
+        let urlSession = URLSession.shared
+        
+        // Try to fetch a new joke
+        // It might not work, so we use a do-catch block
+        do {
+            
+            // Get the raw data from the endpoint
+            let (data, _) = try await urlSession.data(for: request)
+            
+            // Attempt to decode the raw data into a Swift structure
+                                            
+            currentDog = try JSONDecoder().decode(.self, from: data)
+            
+            // Reset the flag that tracks whether the current joke
+            // is a favourite
+            currentDogAddedToSaved = false
+            
+        } catch {
+            print("Could not retrieve or decode the JSON from endpoint.")
+            // Print the contents of the "error" constant that the do-catch block
+            // populates
+            print(error)
+        }
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
